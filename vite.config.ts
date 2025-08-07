58e5a2b038e33212cb08dd4ce44624799434c119
JohnTheParasite <ivan.barbashov@gmail.com>
2025-08-07 12:56:52 +0300

    Update Vite configuration and HTML references; remove unused CSS and JS files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/react-drag-n-drop-todo/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
